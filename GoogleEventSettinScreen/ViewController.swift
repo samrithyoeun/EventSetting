1bec70e54eb8d2240ebcc56abf221a0bebaee58e
sam yoeun <sam.yoeun@pathmazing.com>
2019-07-10 12:01:23 +0700

    Initial Commit

---
+++ GoogleEventSettinScreen/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GoogleEventSettinScreen
+//
+//  Created by Samrith Yoeun on 7/10/19.
+//  Copyright © 2019 Sammi Yoeun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
